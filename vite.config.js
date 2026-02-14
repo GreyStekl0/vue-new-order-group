36b31448a9c578973a387df683b9605c852d5d5d
Maxim Dubrovsky <maxdubmors@gmail.com>
2026-02-14 20:28:24 +0500

    local work in vpn

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    host: '0.0.0.0', // Прослушивание на всех сетевых интерфейсах
+    port: 5173,
+    strictPort: false,
   },
 })
